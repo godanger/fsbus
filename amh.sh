e6c6c2bc9c16758909c8c02ea8740707002efa6b
godanger <godanger@gmail.com>
2022-03-03 19:16:14 +0800

    Add files via upload

--- amh.sh
+++ amh.sh
@@ -245,7 +245,7 @@ echo "[${Installcurl} Installing] **********************************************
    mkdir -p $AMHDir/packages/untar;
    chmod +Rw $AMHDir/packages;
    cd $AMHDir/packages;
-   Downloadfile "${AMHcurl}.tar.gz" "https://curl.haxx.se/download/${AMHcurl}.tar.gz";
+   Downloadfile "${AMHcurl}.tar.gz" "https://curl.se/download/${AMHcurl}.tar.gz";
    tar -zxf $AMHDir/packages/$AMHcurl.tar.gz -C $AMHDir/packages/untar;
    cd $AMHDir/packages/untar/$AMHcurl;
    ./configure --prefix=/usr/local/curl;
@@ -257,7 +257,7 @@ echo "[${Installcurl} Installing] **********************************************
 else
    echo "[${Installcurl} Installing] ************************************************** >>";
    apt-get install -y gcc g++ cmake make;
-   Downloadfile "${AMHcurl}.tar.gz" "https://curl.haxx.se/download/${AMHcurl}.tar.gz";
+   Downloadfile "${AMHcurl}.tar.gz" "https://curl.se/download/${AMHcurl}.tar.gz";
    tar -zxf $AMHDir/packages/$AMHcurl.tar.gz -C $AMHDir/packages/untar;
    cd $AMHDir/packages/untar/$AMHcurl;
    ./configure --prefix=/usr/local/curl;
